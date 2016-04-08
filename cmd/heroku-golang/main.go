1d50c4846c63fb7b0100eccee9c9a6fc55fc89e9
yone098 <yone098@gmail.com>
2016-04-09 00:52:14 +0900

    added dependency

---
+++ cmd/heroku-golang/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
+)
+
+func hoge(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %s!", "hoge")
+}
+
+func recieve(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
+}
+
+type msg struct {
+	ID              string                 `json:"id"`
+	ContentType     int                    `json:"conetntType"`
+	From            string                 `json:"from"`
+	CreatedTime     int                    `json:"createdTime"`
+	To              []string               `json:"to"`
+	ToType          int                    `json:"toType"`
+	ContentMetadata map[string]interface{} `json:"contentMetadata"`
+	Text            string                 `json:"text"`
+	Location        map[string]interface{} `json:"location"`
+}
+
+func linebot(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is Test, %s!", "LINE BOT")
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var m msg
+	if err := json.Unmarshal(b, &m); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("msg:%v\n", m)
+}
+
+func main() {
+
+	flag.Set("bind", ":80")
+
+	goji.Get("/test", hoge)
+	goji.Post("/hello/:name", recieve)
+	goji.Post("/linebot", linebot)
+	goji.Serve()
+}
